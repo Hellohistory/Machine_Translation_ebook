487d908e09475d0bea825d3928f57520fe05f61e
Hellohistory <85825011+Hellohistory@users.noreply.github.com>
2023-08-23 21:34:15 +0800

    修复日志模块两个问题： 1.日志记载文件不匹配 2.重复记载

--- config/logger_config.py
+++ config/logger_config.py
@@ -1,9 +1,18 @@
 import logging
 
+class LevelFilter(logging.Filter):
+    def __init__(self, level):
+        super().__init__()  # 调用超类的__init__方法
+        self.level = level
+
+    def filter(self, record):
+        return record.levelno == self.level
+
 def setup_logger():
-    # 创建一个日志记录器
     logger = logging.getLogger()
-    logger.setLevel(logging.INFO)  # 设置记录器的默认级别
+    logger.handlers = []  # 清除现有的处理器
+
+    logger.setLevel(logging.DEBUG)  # 设置记录器的默认级别
 
     # 创建不同级别的处理器
     debug_handler = logging.FileHandler('utils/logger/debug.log', encoding='utf-8') # 调试日志
@@ -11,11 +20,17 @@ def setup_logger():
     warning_handler = logging.FileHandler('utils/logger/warning.log', encoding='utf-8') # 警告日志
     error_handler = logging.FileHandler('utils/logger/error.log', encoding='utf-8') # 错误日志
 
-    # 设置不同级别的处理器级别
-    debug_handler.setLevel(logging.DEBUG)
-    info_handler.setLevel(logging.INFO)
-    warning_handler.setLevel(logging.WARNING)
-    error_handler.setLevel(logging.ERROR)
+    # 创建级别过滤器
+    debug_filter = LevelFilter(logging.DEBUG)
+    info_filter = LevelFilter(logging.INFO)
+    warning_filter = LevelFilter(logging.WARNING)
+    error_filter = LevelFilter(logging.ERROR)
+
+    # 为处理器添加过滤器
+    debug_handler.addFilter(debug_filter)
+    info_handler.addFilter(info_filter)
+    warning_handler.addFilter(warning_filter)
+    error_handler.addFilter(error_filter)
 
     # 创建格式化器
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
@@ -32,9 +47,8 @@ def setup_logger():
     logger.addHandler(warning_handler)
     logger.addHandler(error_handler)
 
-    # 返回配置好的日志记录器
     return logger
+
 
 # 使用配置好的日志记录器
 logger = setup_logger()
-
